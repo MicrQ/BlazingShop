63834fb72d3e5c6d05be5f336bdb7e74ad7a4850
MicrQ <mail.micrq@proton.me>
2024-07-12 10:05:38 +0300

    filtering by category implemented for each category

--- BlazingShop.Client/obj/Debug/net8.0/BlazingShop.Client.AssemblyInfo.cs
+++ BlazingShop.Client/obj/Debug/net8.0/BlazingShop.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingShop.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+060a419dccd3b81180456db782a3c9d64773bc6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b04cf46741aab66237feca07c5ee3cca03aa83df")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingShop.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingShop.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
